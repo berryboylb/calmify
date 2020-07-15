fffc17a51409942f619f8d619589ccfaacdf876b
berryboylb <phemmynesce4life@gmail.com>
2020-07-15 04:31:13 +0100

    new changes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
-});
+// hamburger.addEventListener("click", () => {
+//   navLinks.classList.toggle("open");
+//   links.forEach(link => {
+//     link.classList.toggle("fade");
+//   });
+// });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("nav", {duration:1, y: '-70px', ease:'bounce'})
+gsap.from('.content1', {duration:1, x: '-100%', ease: 'power2'})
+gsap.from('.content2', {duration:1, x: '100%', scale: 2, ease: 'power2'})
